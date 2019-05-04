4e09325d552e2982bfa3c1f33e58e71e7cd50a46
Elkeborr <elkeborreij@student.thomasmore.be>
2019-05-04 11:54:32 +0200

    signup_werkt_lokaal

---
+++ public/javascripts/signup.js
@@ -0,0 +1,26 @@
+let btnSignup = document.querySelector('.form--btn').addEventListener('click', click =>{
+
+    let username= document.getElementById('email').value;
+    let password=document.getElementById('password').value;
+    
+    fetch ('http://localhost:3000/users/signup',{
+        method:"post",
+        headers: 
+            {'Content-Type':'application/json'},
+            body:JSON.stringify({
+                "username":username,
+                "password":password
+            })
+        }).then(response => {
+            return response.json();
+        }).then (json => {
+            if (json.status==="succes"){
+                let feedback= document.querySelector('.alert');
+                feedback.textContent="sign up complete";
+                feedback.classList.remove('hidden');
+            }
+        });
+
+
+
+});
