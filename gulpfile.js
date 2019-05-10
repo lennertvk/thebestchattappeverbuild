4a0eeaccabc88036e85c8200b0997aef31c331d5
Elkeborr <elkeborreij@student.thomasmore.be>
2019-05-10 11:21:15 +0200

    update

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,16 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
 function sass2css(done){ //done is in de docs cb
-    return src("./source/app.scss") //want alles van de sccs is hierin gelinkt
+    return src("./public/source/app.scss") //want alles van de sccs is hierin gelinkt
 
     //inpluggen dat sass naar css omzet
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(dest("./dist/css/"))
+    .pipe(dest("./public/dist/css/"))
     .pipe(browserSync.stream());
 
     done();
 }
 
-watch("./src/sass/**/*.scss", sass2css);
+watch("./public/source/**.scss", sass2css);
 
 module.exports.default = parallel(sass2css);
