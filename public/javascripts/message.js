ec972a8148abcda799ed123c79e682c44856c0ed
Lennert Van Kerckhove <lennert.van.kerckhove@gmail.com>
2019-05-23 13:43:49 +0200

    ...

--- public/javascripts/message.js
+++ public/javascripts/message.js
@@ -21,7 +21,7 @@ document.getElementById('input').addEventListener('keypress', function (e) {
     var key = e.which || e.keyCode;
     if (key === 13) { // 13 is enter
         let input = document.getElementById('input').value;
-        that.primus.write({
+        primus.write({
             "action": "clicked",
             "input" : input
         });
