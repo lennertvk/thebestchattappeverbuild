1eab2e3115752d351c75d66f51519cf5d294327b
Lennert Van Kerckhove <lennert.van.kerckhove@gmail.com>
2019-05-23 13:35:24 +0200

    fixen primus messages

---
+++ public/javascripts/message.js
@@ -0,0 +1,26 @@
+var primus = Primus.connect("/", {
+    reconnect: {
+        max: Infinity // Number: The max delay before we try to reconnect.
+            ,
+        min: 500 // Number: The minimum delay before we try reconnect.
+            ,
+        retries: 10 // Number: How many times we should try to reconnect.
+    }
+});
+primus.on("data", function (data) {
+    if(data.action == "clicked"){
+        console.log(data.input);
+    }
+});
+
+document.getElementById('input').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) { // 13 is enter
+        let input = document.getElementById('input').value;
+        that.primus.write({
+            "action": "clicked",
+            "input" : input
+        });
+        e.preventDefault();
+    }
+});
